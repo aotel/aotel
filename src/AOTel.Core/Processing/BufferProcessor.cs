3090c9add5e283de443f93a3d7fefa06e3860411
Abdelghani Moussaid <20841568+abdelghani-moussaid@users.noreply.github.com>
2026-05-12 20:50:28 +0100

    refactor: eliminate flattening in OtlpTraceReader and BufferProcessor

--- src/AOTel.Core/Processing/BufferProcessor.cs
+++ src/AOTel.Core/Processing/BufferProcessor.cs
@@ -4,7 +4,7 @@ using AOTel.Core.Parsing;
 namespace AOTel.Core.Processing;
 
 /// <summary>
-/// Handles the zero-allocation flattening of Kestrel's ReadOnlySequence
+/// Handles the zero-allocation processing of Kestrel's ReadOnlySequence
 /// and bridges the raw socket memory to the Protobuf reader.
 /// </summary>
 public static class BufferProcessor
@@ -12,53 +12,18 @@ public static class BufferProcessor
     public static bool Process<TProcessor>(ReadOnlySequence<byte> buffer, ref TProcessor processor)
         where TProcessor : struct, ISpanProcessor
     {
-        if (buffer.IsSingleSegment)
+        int length = (int)buffer.Length;
+        if (length > 1024 * 1024 * 5)
         {
-            var otlpReader = new OtlpTraceReader(buffer.FirstSpan);
-            foreach (var span in otlpReader)
-            {
-                processor.Process(in span);
-            }
-
-            return true;
+            return false;
         }
-        else
+
+        var otlpReader = new OtlpTraceReader(buffer);
+        foreach (var span in otlpReader)
         {
-            int length = (int)buffer.Length;
-            if (length > 1024 * 1024 * 5)
-            {
-                return false;
-            }
-
-            if (length <= 4096)
-            {
-                Span<byte> stackBuffer = stackalloc byte[length];
-                buffer.CopyTo(stackBuffer);
-                var otlpReader = new OtlpTraceReader(stackBuffer);
-                foreach (var span in otlpReader)
-                {
-                    processor.Process(in span);
-                }
-            }
-            else
-            {
-                byte[] rented = ArrayPool<byte>.Shared.Rent(length);
-                try
-                {
-                    buffer.CopyTo(rented);
-                    var otlpReader = new OtlpTraceReader(rented.AsSpan(0, length));
-                    foreach (var span in otlpReader)
-                    {
-                        processor.Process(in span);
-                    }
-                }
-                finally
-                {
-                    ArrayPool<byte>.Shared.Return(rented);
-                }
-            }
-
-            return true;
+            processor.Process(in span);
         }
+
+        return true;
     }
 }
